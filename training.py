30af8a486be123d3c4125a2403bee89f6c64ad5e
Nicolas BLED <nbled@made-f0Br4s8.clusters.42paris.fr>
2024-06-24 14:38:54 +0200

    theta normalized

--- training.py
+++ training.py
@@ -1,12 +1,21 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Global normalization variables
+km_mean = 0
+km_std = 0
+price_mean = 0
+price_std = 0
+
 def write_parameters(file_path, theta0, theta1):
+    theta1_denormalized = theta1 * price_std / km_std
+    theta0_denormalized = price_mean + theta0 * price_std - theta1_denormalized * km_mean
     with open(file_path, 'w') as file:
-        file.write(f"{theta0}\n")
-        file.write(f"{theta1}\n")
+        file.write(f"{theta0_denormalized}\n")
+        file.write(f"{theta1_denormalized}\n")
 
 def normalize(data):
+    global km_mean, km_std, price_mean, price_std
     km_mean = data['km'].mean()
     km_std = data['km'].std()
     price_mean = data['price'].mean()
@@ -15,7 +24,13 @@ def normalize(data):
     data['km_normalized'] = (data['km'] - km_mean) / km_std
     data['price_normalized'] = (data['price'] - price_mean) / price_std
     
-    return data, km_mean, km_std, price_mean, price_std
+    return data
+
+def denormalize_line(x_values, line_y_normalized):
+    global km_mean, km_std, price_mean, price_std
+    x_values_denormalized = x_values * km_std + km_mean
+    line_y_denormalized = line_y_normalized * price_std + price_mean
+    return x_values_denormalized, line_y_denormalized
 
 def training(data, learning_rate, iteration):
     views = []
@@ -32,40 +47,33 @@ def training(data, learning_rate, iteration):
         size = len(data)
 
         for km, price in zip(x_values, y_values):
-            current_diff = (theta0 * km + theta1) - price
-            total_diff_theta0 += current_diff * km
-            total_diff_theta1 += current_diff
+            current_diff = (theta0 + theta1 * km) - price
+            total_diff_theta0 += current_diff
+            total_diff_theta1 += current_diff * km
         
         theta0 -= total_diff_theta0 / size * learning_rate
         theta1 -= total_diff_theta1 / size * learning_rate
 
-        line_y_normalized = theta0 * x_values + theta1
+        line_y_normalized = theta0 + theta1 * x_values
         
         views.append((x_values, line_y_normalized))
 
     parameter_file = 'parameters.txt'
     write_parameters(parameter_file, theta0, theta1)
 
-    return views, theta0, theta1
+    return views
 
-def denormalize_line(x_values, line_y_normalized, km_mean, km_std, price_mean, price_std):
-    x_values_denormalized = x_values * km_std + km_mean
-    line_y_denormalized = line_y_normalized * price_std + price_mean
-    return x_values_denormalized, line_y_denormalized
-
-def display_plot(views, data, km_mean, km_std, price_mean, price_std, a, b):
+def display_plot(views, data):
     current_view = [0]
 
     def update_plot(view_index):
         ax.clear()
         x_values, line_y_normalized = views[view_index]
-        x_values_denormalized, line_y_denormalized = denormalize_line(
-            x_values, line_y_normalized, km_mean, km_std, price_mean, price_std
-        )
+        x_values_denormalized, line_y_denormalized = denormalize_line(x_values, line_y_normalized)
         
         ax.plot(x_values_denormalized, line_y_denormalized, label=f'Iteration {view_index + 1}')
         
-        ax.scatter(data['km'], data['price'], color='red', label='Data Points   ')
+        ax.scatter(data['km'], data['price'], color='red', label='Data Points')
         
         ax.set_title('Car Price vs Kilometers Driven')
         ax.set_xlabel('Kilometers Driven')
@@ -79,7 +87,7 @@ def display_plot(views, data, km_mean, km_std, price_mean, price_std, a, b):
 
     def on_key(event):
         if event.key == ' ':
-            current_view[0] = (current_view[0] + 1) % len(views)
+            current_view[0] = (current_view[0] + 10) % len(views)
             update_plot(current_view[0])
 
     fig.canvas.mpl_connect('key_press_event', on_key)
@@ -88,9 +96,9 @@ def display_plot(views, data, km_mean, km_std, price_mean, price_std, a, b):
 
 def main():
     data = pd.read_csv('data.csv')
-    data, km_mean, km_std, price_mean, price_std = normalize(data)
-    views, a, b = training(data, 0.01, 1000)
-    display_plot(views, data, km_mean, km_std, price_mean, price_std, a, b)
+    data = normalize(data)
+    views = training(data, 0.001, 10000)
+    display_plot(views, data)
 
 if __name__ == "__main__":
     try:
